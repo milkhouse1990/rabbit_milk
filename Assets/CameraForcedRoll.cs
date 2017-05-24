7f6ddf8a55ea7113a78c3bfca42a4c9f1b5cf514
milkhouse <kurokun@163.com>
2017-05-24 11:20:16 +0800

    dv0.1.9-170524
    demo框架（挖坑）

---
+++ Assets/CameraForcedRoll.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraForcedRoll : MonoBehaviour {
+
+    private GameObject milk;
+    private Vector2 scroll_velocity = new Vector2(4f, 0);
+
+	// Use this for initialization
+	void Start () {
+        transform.position = new Vector3(10, 5.625f, -10);
+        if ((milk = GameObject.Find("milk")) == null)
+            Debug.Log("cant find milk");
+        GetComponent<Rigidbody2D>().velocity = scroll_velocity;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.x < 20)
+        {
+            //force velocity
+            milk.GetComponent<Rigidbody2D>().velocity+= new Vector2(4f,0);
+        }
+        else
+        {
+            GetComponent<CameraFollow>().enabled = true;
+            enabled=false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        }
+            
+    }
+}
