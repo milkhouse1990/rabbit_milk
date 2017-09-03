eddc15ffc586193542edcb21250e023b228bd5be
milkhouse_pc <kurokun@163.com>
2017-09-03 20:12:01 +0800

    dv0.1.17
    菜单调整

--- Assets/DataBase.cs
+++ Assets/DataBase.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using System.Text;
 using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
@@ -13,32 +14,90 @@ public class DataBase : MonoBehaviour {
     private string[] item;
     private bool[][] b_item;
     public Texture2D vector;
-    private string[][] info;
-    private int pos = 0;
+    private string[] info;
+    private int[] pos = { 0, 0, 0};
+    private int[] dispos = { 0, 0, 0 };
     private int labelpos = 0;
     private int cospos = 0;
-    private int dispos = 0;
     private int labels;
     private bool b_info=true;
 
-    public GameObject List;
-    private GameObject pages;
+    public List list;
+    private List page;
 
     // Use this for initialization
     void Start () {
         labels = labelname.Length;
         item = new string[labels];
-        item[0] = "milk酱\n公主\n3\n4\n5\n6\n7\n8\n9\n10\n11";
+
         item[1]= "1\n2";
         item[2] = "Happy Ending\n最速Clear\n地狱Ending";
 
+        //string raw="milk酱\n公主\n233\n4\n5\n6\n7\n8\n9\n10\n11";
+
         b_item = new bool[labels][];
-        string path = "Save\\database.txt";
+
+        FileStream fs;
+        string mes;
+
+        string path = "Text\\mnu.txt";
+
+        string[] readins= File.ReadAllLines(path);
+        string raw = "";
+        int k = 0;
+        foreach (string readin in readins)
+        {
+            k++;
+            if (readins.Length > k)
+                raw += readin + "\n";
+            else
+                raw += readin;
+        }
+            
+        path= "Text\\temp.txt";
+        fs = new FileStream(path, FileMode.OpenOrCreate);
+        StreamWriter sw = new StreamWriter(fs);
+        sw.Write(raw);
+        sw.Flush();
+        sw.Close();
+        fs.Close();
+
+        fs = new FileStream(path, FileMode.OpenOrCreate);
+        byte[] bytes = new byte[fs.Length];
+        fs.Read(bytes, 0, (int)fs.Length);
+        
+        path = "Text\\mnu.bin";
+        FileStream fsout = new FileStream(path, FileMode.OpenOrCreate);
+        fsout.Write(bytes, 0, (int)bytes.Length);
+        fsout.Flush();
+        fsout.Close();
+        fs.Close();
+
+        if (File.Exists(path))
+        {
+            fs = new FileStream(path, FileMode.Open);
+            byte[] bytes2 = new byte[fs.Length];
+            fs.Read(bytes2, 0, (int)fs.Length);
+            fs.Close();
+            Decoder d = Encoding.UTF8.GetDecoder();
+            char[] chars=new char[bytes2.Length];
+            d.GetChars(bytes2, 0, bytes2.Length, chars, 0);
+            Debug.Log(chars[0]);
+            item[0] = new string(chars);
+
+        }
+        else
+            Debug.Log("game file lost: " + path);
+ 
+
+
+        path = "Save\\database.txt";
+        
         if (!File.Exists(path))
         {
             //File.Create(path);
-            FileStream fs = new FileStream(path, FileMode.Create);
-            StreamWriter sw = new StreamWriter(fs);
+            fs = new FileStream(path, FileMode.Create);
+            sw = new StreamWriter(fs);
             for (int i = 0; i < labels; i++)
             {
                 sw.Write("0");
@@ -66,29 +125,13 @@ public class DataBase : MonoBehaviour {
                     b_item[i][j] = false;
         }
         
-        info = new string[labels][];
-        info[0] = new string[]
-        {
-            "种族不明\n本作主人公。",
-            "兔耳族\n王国的公主殿下。",
-            "3"
-        };
-        info[1] = new string[]
-        {
-            "1",
-            "2",
-            "3"
-        };
-        info[2] = new string[]
-        {
-            "触发条件：按照正常流程攻略\n牛奶酱救回了公主，打跑了坏人，王国又恢复了和平。",
-            "触发条件：生日Party上在和对话后攻击她\n牛奶酱提前识破了坏人的阴谋，阻止了一切的发生。",
-            "触发条件：Miss100次\n不管怎样都过不了关，于是死后在地狱和???过上了幸福的生活。"
-        };
+        info = new string[labels];
+        info[0] = "种族不明 本作主人公。\n兔耳族 王国的公主殿下。\n3\n\n\n\n\n\n\n\n";
+        info[1] = "1\n2\n3";
+        info[2] = "触发条件：按照正常流程攻略 牛奶酱救回了公主，打跑了坏人，王国又恢复了和平。\n触发条件：生日Party上在和对话后攻击她 牛奶酱提前识破了坏人的阴谋，阻止了一切的发生。\n触发条件：Miss100次 不管怎样都过不了关，于是死后在地狱和???过上了幸福的生活。";
 
         //message lock manage
-        string[] messages;
-        
+        string[] messages;      
             
         for (int i=0;i<item.Length;i++)
         {
@@ -98,77 +141,56 @@ public class DataBase : MonoBehaviour {
             {
                 if (!b_item[i][j])
                     messages[j] = "???";
-                item[i] += messages[j]+"\n";
-            }                 
+                item[i] += messages[j];
+                if (j!=messages.Length-1)
+                    item[i]+="\n";
+            }
+
+            messages = info[i].Split('\n');
+            info[i] = "";
+            for (int j = 0; j < messages.Length; j++)
+            {
+                if (!b_item[i][j])
+                    messages[j] = "???????";
+                info[i] += messages[j];
+                if (j != messages.Length - 1)
+                    info[i] += "\n";
+            }
         }
 
         {
-            pages= Instantiate(List);
-            pages.GetComponent<List>().Init(item[labelpos]);
-            //temp = Instantiate(List);
-            //temp.GetComponent<List>().Init("1\n2\n3\n4");
-            /*
-            pages[i].GetComponent<List>().Init(new string[]
-            {
-            "1",
-            "2",
-            "3"
-            });*///pages[i].GetComponent<List>().items[0] = "0";
+            page = Instantiate<List>(list);
+            page.Init(vector);
+            page.InitText(item[labelpos],info[labelpos]);
         }
-
-
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("db.update");
-        if (CrossPlatformInputManager.GetButtonDown("down")||
-            (CrossPlatformInputManager.GetButton("down") && delay==0))
-        {
-            delay = 10;
-            pos++;
-            if (pos - dispos == itemperscr)
-                dispos++;
-            if (pos == item[labelpos].Length)
-            {
-                pos = 0;
-                dispos = 0;
-            }               
-        }
-        if (CrossPlatformInputManager.GetButtonDown("up") || 
-            (CrossPlatformInputManager.GetButton("up") && delay==0))
-        {
-            delay = 10;
-            pos--;
-            if (pos < dispos)
-                dispos--;
-            if (pos == -1)
-            {
-                //pos = item[labelpos].Length - 1;
-                Debug.Log(pos);
-                dispos = pos - itemperscr+1;
-            }               
-        }
         if (CrossPlatformInputManager.GetButtonDown("L"))
         {
-            pos = 0;
-            dispos = 0;
+            pos[labelpos] = page.GetFocus();
+            dispos[labelpos] = page.GetScroll();
             labelpos--;
             if (labelpos == -1)
                 labelpos = labels-1;
-            pages.GetComponent<List>().Init(item[labelpos]);
+            page.InitText(item[labelpos],info[labelpos]);
+            page.SetFocus(pos[labelpos]);
+            page.SetScroll(dispos[labelpos]);
         }
         if (CrossPlatformInputManager.GetButtonDown("R"))
         {
-            pos = 0;
-            dispos = 0;
+            pos[labelpos] = page.GetFocus();
+            dispos[labelpos] = page.GetScroll();
             labelpos++;
             if (labelpos == labels)
                 labelpos = 0;
-            pages.GetComponent<List>().Init(item[labelpos]);
+            page.InitText(item[labelpos],info[labelpos]);
+            page.SetFocus(pos[labelpos]);
+            page.SetScroll(dispos[labelpos]);
         }
         if (CrossPlatformInputManager.GetButtonDown("A"))
-            b_info = !b_info;
+            page.SetBInfo(!page.GetBInfo());
         if (CrossPlatformInputManager.GetButtonDown("X"))
             cospos++;           
     }
@@ -179,31 +201,10 @@ public class DataBase : MonoBehaviour {
     }
     void OnGUI()
     {
-        string dis;
-        //list
-        for (int i=dispos;i<dispos+itemperscr;i++)
-            if (i<item[labelpos].Length)
-            {              
-                //string dis;
-                
-                //Debug.Log(dis);
-                //GUI.Label(new Rect(0, 0, 640, 20), dis);
-                //GUI.Label(new Rect(100, 100+(i - dispos) * 20, 640, 20), dis);
-            }
-        //GUI.Label(new Rect(80, 100 + (pos - dispos) * 20, 20, 20), vector);
+        string dis="<color=black>"+ "↑↓项目选择 LR标签选择 A文字表示/非表示 X服装变更"+"</color>";
+                            
         //help
-        GUI.Label(new Rect(1280 - 400, 100, 400, 20), "↑↓项目选择 LR标签选择 A文字表示/非表示 X服装变更");
-        //info
-        /*
-        if (b_info)
-        {
-            Debug.Log(pos);
-            if (b_item[labelpos][pos])
-                //Debug.Log(info[0][0]);
-                dis = info[labelpos][pos];
-            else
-                dis = "???????";
-            GUI.Label(new Rect(640, 360, 640, 360), dis);
-        }*/
+        GUI.Label(new Rect(1280 - 400, 100, 400, 20), dis);
+
     }
 }
--- Assets/List.cs
+++ Assets/List.cs
@@ -4,38 +4,49 @@ using UnityEngine;
 using UnityStandardAssets.CrossPlatformInput;
 
 public class List : MonoBehaviour {
-    private bool ready = false;
-
+    
     private int item = 0;
     private int delay = 0;
     private string pre_items;
+    private string pre_infos;
     private string[] items;
+    private string[] infos;
+    private Texture2D vector;
+    private bool b_info=true;
+
     public Rect pos;
     public int vspace;
+    
+
     private int itemperscr = 10;
     private int dispos = 0;
 
-    public void Init(string p_pre_items)
+    public void Init(Texture2D p_vector)
+    {
+        vector = p_vector;
+    }
+    public void InitText(string p_pre_items, string p_pre_infos)
     {
         pre_items = p_pre_items;
         items = pre_items.Split('\n');
-        ready = true;
+
+        pre_infos = p_pre_infos;
+        infos = pre_infos.Split('\n');
     }
     // Use this for initialization
     void Start()
     {
-        Debug.Log("list.start");
-        //Init("1\n2");
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
-        Debug.Log("list.update");
-        if (ready)
+       
         {
             if (CrossPlatformInputManager.GetButtonDown("down") ||
             (CrossPlatformInputManager.GetButton("down") && delay == 0))
             {
+                
                 delay = 10;
                 item++;
                 if (item - dispos == itemperscr)
@@ -69,6 +80,7 @@ public class List : MonoBehaviour {
     }
     void OnGUI()
     {
+        //text
         string dis = "";
         //for (int i = 0; i < m_item; i++)
         //GUI.Label(pos, list_text);
@@ -77,9 +89,43 @@ public class List : MonoBehaviour {
             if (i == item)
                 dis = "<color=magenta>" + items[i] + "</color>";
             else
-                dis = items[i];
+                dis = "<color=black>" + items[i] + "</color>";
             GUI.Label(new Rect(pos.x, pos.y + (i - dispos) * vspace, pos.width, pos.width), dis);
         }
-        
+
+        //vector
+        GUI.Label(new Rect(pos.x-20, pos.y + (item - dispos) * vspace, 20, 20), vector);
+
+        //info
+        if (b_info)
+        {
+            dis = "<color=black>" + infos[item] + "</color>";
+            GUI.Label(new Rect(640, 360, 640, 360), dis);
+        }
+    }
+
+    public int GetFocus()
+    {
+        return item;
+    }
+    public void SetFocus(int p_focus)
+    {
+        item = p_focus;
+    }
+    public int GetScroll()
+    {
+        return dispos;
+    }
+    public void SetScroll(int p_scroll)
+    {
+        dispos = p_scroll;
+    }
+    public bool GetBInfo()
+    {
+        return b_info;
+    }
+    public void SetBInfo(bool p_b_info)
+    {
+        b_info = p_b_info;
     }
 }
