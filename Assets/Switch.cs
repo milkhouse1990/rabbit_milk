be9741475f37daa414cb38ce5c46aae278736cc8
milkhouse_pc <kurokun@163.com>
2017-11-03 17:35:12 +0800

    idol boss demo 0.1
    boss前的准备工作

---
+++ Assets/Switch.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+    private Transform switch_up;
+    private Transform switch_down;
+    private Transform platform;
+    private bool working;
+    private bool up;
+	// Use this for initialization
+	void Start () {
+        switch_up = transform.Find("switch_up");
+        switch_down = transform.Find("switch_down");
+        platform = transform.Find("platform");
+        working = false;
+        up = GetComponent<Animator>().GetBool("up");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (platform.position.y>14)
+        {
+            platform.position = new Vector3(platform.position.x, 14f, platform.position.z);
+            platform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            working = false;
+        }
+        if (platform.position.y<3.25)
+        {
+            platform.position = new Vector3(platform.position.x, 3.25f, platform.position.z);
+            platform.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            working = false;
+        }
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!working)
+        {
+            
+            if (other.tag == "weapon")
+            {
+                up = !up;
+                GetComponent<Animator>().SetBool("up", up);
+                working = true;
+                if (up)
+                    platform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -2f);
+                else
+                    platform.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 2f);
+            }
+                
+        }
+        
+
+    }
+}
