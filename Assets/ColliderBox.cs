269e296cc3d42775f87534142bd59f3d4caf0a4f
milkhouse <kurokun@163.com>
2017-12-21 00:00:38 +0800

    171220

--- Assets/ColliderBox.cs
+++ Assets/ColliderBox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ColliderBox : MonoBehaviour
 {
+    public int type;
     public Rect size;// from origin
     // Use this for initialization
     void Start()
@@ -20,11 +21,26 @@ public class ColliderBox : MonoBehaviour
     {
         Rect4 collider = new Rect4(size);
         collider = new Rect4(transform.position.y + collider.up, transform.position.y + collider.down, transform.position.x + collider.left, transform.position.x + collider.right);
-        if (position.x >= collider.left)
-            if (position.x <= collider.right)
-                if (position.y <= collider.up)
-                    if (position.y >= collider.down)
-                        return true;
+        switch (type)
+        {
+            // box
+            case 0:
+                if (position.x >= collider.left)
+                    if (position.x <= collider.right)
+                        if (position.y <= collider.up)
+                            if (position.y >= collider.down)
+                                return true;
+                return false;
+            // rightdown
+            case 1:
+                if (position.x <= collider.right)
+                    if (position.y > collider.down)
+                        if (position.x > position.y)
+                            return true;
+                return false;
+
+        }
         return false;
+
     }
 }
